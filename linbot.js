0b4d6d687d6bad69f275d1d48eaab9896120314d
Byron <47398200+byronxc@users.noreply.github.com>
2021-02-06 13:21:48 -0800

    added axios

--- linbot.js
+++ linbot.js
@@ -1,6 +1,28 @@
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
+const axios = require('axios');
 const client = new Discord.Client();
+
+const translate = (_message, original) => {
+
+	const config = {
+		headers: {
+			'Ocp-Apim-Subscription-Key': '398d6dd6a9a84dc7b8df5a4295224d3e',
+			'Content-Type': 'application/json',
+		}
+	}
+
+	const body = `[{'Text':'${original}'}]`;
+
+	axios.post(
+		'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=es', 
+		body,
+		config,)
+  .then((response) => {
+	_message.channel.send('done');
+   
+  });
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -9,9 +31,8 @@ client.once('ready', () => {
 client.on('message', message => {
 	if (message.content.toLowerCase() === `hello`) {
 		message.channel.send(`Hey ${message.author} how's it going guys?`);
+		translate(message);
 	}
 });
 
 client.login(token);
-
-//https://discord.com/api/oauth2/authorize?client_id=801952651860115496&scope=bot
