27295561ea5b425e9ac532292f4a581d238a4c8c
Byron <47398200+byronxc@users.noreply.github.com>
2021-02-16 17:47:19 -0800

    added multi language feature

--- linbot.js
+++ linbot.js
@@ -10,18 +10,15 @@ const translate = new Translate({
     projectId: CREDENTIALS.project_id
 });
 
-const target = 'es';
-
-const translateText = async (text) => {
+const translateText = async (text, target) => {
     try {
         let [response] = await translate.translate(text, target);
         return response;
     } catch (error) {
         console.log(`Error at translateText --> ${error}`);
-        return 0;
+        return "Entered a wrong Language";
     }
 };
-
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -29,7 +26,15 @@ client.once('ready', () => {
 
 client.on('message', message => {
 	if(!message.author.bot) {
-		translateText(message.content)
+		text = message.content.split('-')[0];
+		target = message.content.split('-')[1].trim().toLocaleLowerCase();
+
+		if(target == 'spanish')
+			target = 'es'
+		if(target == 'french')
+			target = 'fr'
+
+		translateText(text, target)
 		.then((res) => {
 			message.channel.send(res);
 		})
